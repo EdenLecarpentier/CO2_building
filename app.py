4f908d8c2b2f06c70fb3683905e1d563712dbf44
Thefubuking <eden.lecarpentier.simplon@gmail.com>
2022-06-08 11:38:07 +0200

    main

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 import pandas as pd
 import streamlit as st
 import os
-
+import pickle
 
 st.title("""
 # Home Credit Default Risk
 """)
 
 uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
 #if uploaded_file is not None:
 #    df = pd.read_csv(uploaded_file)
 #    df.drop('Unnamed: 0', inplace=True, axis=1)
@@ -22,3 +24,7 @@ uploaded_file = st.file_uploader("Choose a file")
 #    st.line_chart(df)
 
 MODEL_DIR = os.path.join(os.path.dirname('file'), 'model.pickle')
+with open(MODEL_DIR , 'rb') as handle:
+    model = pickle.load(handle)
+st.title("Time for a Prediction")
+st.button("test")
