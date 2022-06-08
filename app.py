2b3e0a5d978ee1838760f37cdb7f97b346993c86
Thefubuking <eden.lecarpentier.simplon@gmail.com>
2022-06-08 11:05:34 +0200

    t

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 
-import numpy as np
 import pandas as pd
 import streamlit as st
-import os 
+
 
 
 st.title("""
@@ -10,15 +9,15 @@ st.title("""
 """)
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    df.drop('Unnamed: 0', inplace=True, axis=1)
-    st.write(df.head())
-    #Select ID
-    st.selectbox('Choose ID', df)
-    #Graphique
-    df = pd.DataFrame(
-        np.random.randn(20, 3),
-        columns=['EXT_SOURCE_1', 'EXT_SOURCE_2','EXT_SOURCE_3'])
-    st.line_chart(df)
+#if uploaded_file is not None:
+#    df = pd.read_csv(uploaded_file)
+#    df.drop('Unnamed: 0', inplace=True, axis=1)
+#    st.write(df.head())
+#    #Select ID
+#    st.selectbox('Choose ID', df)
+#    #Graphique
+#    df = pd.DataFrame(
+#        np.random.randn(20, 3),
+#        columns=['EXT_SOURCE_1', 'EXT_SOURCE_2','EXT_SOURCE_3'])
+#    st.line_chart(df)
 
