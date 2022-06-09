549641de5166a4263ef7e65cd6e3fb1c6ec4ffd7
Thefubuking <eden.lecarpentier.simplon@gmail.com>
2022-06-09 15:22:24 +0200

    main

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import streamlit as st
 import os
 import pickle
 
+
 st.title("""
-# Home Credit Default Risk
+# Co2 Building
 """)
 
 #if uploaded_file is not None:
@@ -26,28 +27,23 @@ if uploaded_file is not None:
             #test_x = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
             val = model.predict(uploaded_file)
             #.reshape(-1, 28, 28,1))
-#    #Select ID
-#    st.selectbox('Choose ID', df)
-#    #Graphique
-#    df = pd.DataFrame(
-#        np.random.randn(20, 3),
-#        columns=['EXT_SOURCE_1', 'EXT_SOURCE_2','EXT_SOURCE_3'])
+          
+            st.write(val)
 
 
-
-original_list = ["None","Office" , "Parking" , "Other" , "Retail Store" , "Self-Storage Facility" , "Other-Services" , "Restaurant" , "Supermarket" , "Bar/nightclub" , "Other-Education" , "Data Center" , "Non refregirated warehouse"]
-type_of_building = st.sidebar.selectbox("select your first main building type : " , original_list)
-
-type_of_building = st.sidebar.selectbox("select your second main building type : " , original_list)
-
-type_of_building = st.sidebar.selectbox("select your third main building type : " , original_list)
-
-number_of_floor_list = ["1" , "2" , "3" , "4" , "5"]
-number_of_floor = st.sidebar.selectbox("select the number of floor in your buildings : " , number_of_floor_list)
-
-# year_it_was_built_list = ["2021", "2022"]
-
-date = st.sidebar.date_input('date the building(s) where created :')
-test_x = pd.read_csv("C:/Users/edenl/Desktop/ia_coding/machine_learning/co2/data/Clean.csv")
+#original_list = ["None","Office" , "Parking" , "Other" , "Retail Store" , "Self-Storage Facility" , "Other-Services" , "Restaurant" , "Supermarket" , "Bar/nightclub" , "Other-Education" , "Data Center" , "Non refregirated warehouse"]
+#type_of_building = st.sidebar.selectbox("select your first main building type : " , original_list)
+#
+#type_of_building = st.sidebar.selectbox("select your second main building type : " , original_list)
+#
+#type_of_building = st.sidebar.selectbox("select your third main building type : " , original_list)
+#
+#number_of_floor_list = ["1" , "2" , "3" , "4" , "5"]
+#number_of_floor = st.sidebar.selectbox("select the number of floor in your buildings : " , number_of_floor_list)
+#
+## year_it_was_built_list = ["2021", "2022"]
+#
+#date = st.sidebar.date_input('date the building(s) where created :')
+#test_x = pd.read_csv("C:/Users/edenl/Desktop/ia_coding/machine_learning/co2/data/Clean.csv")
 
                   
