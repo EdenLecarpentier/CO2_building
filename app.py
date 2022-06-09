303ccb4ed67cc05f03f5c1ab16f555c48394ab9f
Thefubuking <eden.lecarpentier.simplon@gmail.com>
2022-06-09 16:42:24 +0200

    main

--- app.py
+++ app.py
@@ -25,10 +25,9 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     if st.button('Predict'):
             #test_x = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        #val = model.predict()
+        val = model.predict(df)
             #.reshape(-1, 28, 28,1))
-        st.table(df)
-
+        st.write(handle)
             
 
 
